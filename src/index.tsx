6211f355458dd6e2c87c85a6a00c1d161d41fc8e
Your Name <mhs.malekan@gmail.com>
2020-08-01 23:59:34 +0430

    add SSO login

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "containers/App";
-import config from "config";
-
-ReactDOM.render(<App appConfig={config} />, document.getElementById("root"));
